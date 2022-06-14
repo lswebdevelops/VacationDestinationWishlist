52cadcb5db79f083bed79e25143f029e8581e904
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-06-13 21:21:15 -0300

    Script working properly

--- script.js
+++ script.js
@@ -4,51 +4,56 @@ destailsForm.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(event){
     event.preventDefault();
-    //1. extract the value from each form field
-   
+      
     
-    let destName = event.target.elements["name"].value;
+    let destName     = event.target.elements["name"].value;
     let destLocation = event.target.elements["location"].value;
-    let destPhoto = event.target.elements["photo"].value;
-    let destDesc = event.target.elements["description"].value;
+    let destPhoto    = event.target.elements["photo"].value;
+    let destDesc     = event.target.elements["description"].value;
     //2. clear out the form fields
     for(let i =0; i < destailsForm.length; i++){
-        destailsForm.elements[i].value ="";
+        destailsForm.elements[i].value = "";
     }
     //3. run a function that creates the new card
 
-    
-    let wishListContainer = document.querySelector("#destinations_container");
+     //5. add the card
+     let destCard = createDestinationCard(destName, destLocation, destPhoto, destDesc);
+
+    let wishListContainer = document.getElementById("destinations_container");
+
     if (wishListContainer.children.length === 0){
-    document.querySelector("#title").innerHTML = "My Wish List";
+    document.getElementById("title").innerHTML = "My Wish List";
     //could also be: 
     //document.getElementById("title").innerHTML = "My Wish List";
     }
 
-    //4. if needed , change the header at the top of the destination list
-
-
-    //5. add the card
-
+    document.querySelector('#destinations_container').appendChild(destCard);
+   
     }
+//conferido 28th page 2x
 
-    function createDestinationCard(name, location, photoUrl, description){
-    let card = document.createElement("div");
-    card.className = "card";
+
+
+
+    function createDestinationCard(name, location, photoURL, description){
+    
+        let card = document.createElement("div");
+         card.className = "card";
 
     let img = document.createElement('img');
     //here the alt always receives the same name as name of the image
     img.setAttribute('alt', name);
-    let constantPhotoUrl = "images/signpost.jpg";
+    let constantphotoUrl = "images/signpost.jpg";
 
-    if(photoUrl.length ===0){
-         img.setAttribute('src', constantPhotoUrl);
+    if(photoURL.length ===0){
+         img.setAttribute('src', constantphotoUrl);
     }
      else{
-        img.setAttribute('src', photoUrl);
+        img.setAttribute('src', photoURL);
     }
 
         card.appendChild(img);
+        //conferido 47 page
 
       let cardBody = document.createElement("div");
      cardBody.className = "card-body";
@@ -58,23 +63,28 @@ function handleFormSubmit(event){
         cardBody.appendChild(cardTitle);
 
      let cardSubtitle = document.createElement('h4');
-        cardTitle.innerText = location;
+     cardSubtitle.innerText = location;
         cardBody.appendChild(cardSubtitle);
+
          if(description.length !==0){
             let cardText = document.createElement('p');
-            cardText.className  ='card-text';
+            cardText.className = 'card-text';
             cardText.innerText = description;
             cardBody.appendChild(cardText);
     
          }
-
+//conferido page 65
 
         let cardDeleteBtn = document.createElement('button');
-        cardTitle.innerText = Remove;
+        cardDeleteBtn.innerText = 'Remove';
         
-            cardDeleteBtn.addEventListener("click", removeDescription);
-            cardDeleteBtn.appendChild(cardDeleteBtn);
-         
+
+
+
+
+            cardDeleteBtn.addEventListener("click", removeDestination);
+            cardBody.appendChild(cardDeleteBtn);
+         //
             card.appendChild(cardBody);
             return card;
 
@@ -82,6 +92,12 @@ function handleFormSubmit(event){
 
 
 
+}
+
+function removeDestination (event){
+    let card = event.target.parentElement.parentElement;
+    // it goes up one parent, then another parent and removes it
+    card.remove();
 }
 
 
